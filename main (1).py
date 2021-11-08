18d9d526113d4fe3d5a3d2c777fea0776f628979
Pedro Pigatto <pedropigatto54@gmail.com>
2021-11-08 18:46:10 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,9 @@
+﻿flower = input("Type a flower name: ")
+
+#here is the conditions according to the input
+if flower == 'Spathiphyllum':
+    print("Spathiphyllum is the best plant ever!")
+elif flower == 'spathiphyllum':
+    print('No, I want a big Spathiphyllum!')
+elif flower != 'Spathiphyllum' and flower != 'spathiphyllum':
+    print('Spathiphyllum! Not ', flower, ' ! ')
