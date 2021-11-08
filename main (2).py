81f7eb0d7f8d3be656416963452d367ec36e2b92
Pedro Pigatto <pedropigatto54@gmail.com>
2021-11-08 18:45:04 -0300

    Add files via upload
    this is a tax calculator using a fictional money |  isso é uma calculadora de impostos usando dinheiro ficticio

---
+++ main (2).py
@@ -0,0 +1,14 @@
+﻿income = float(input("Enter the annual income: "))
+
+if income <= 85528:
+    count = (income * 0.18) - 556.2
+    tax = round(count,0)
+else:
+    count = ((income - 85528) * 0.32) + 14839.2
+    tax = round(count,0)
+
+if tax < 0:
+    tax = 0
+
+#tax = round(tax, 0)
+print("The tax is:", tax, "thalers")
