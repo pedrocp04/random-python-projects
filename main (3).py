b9adf7547fb1d6a126fa42e975d6a59988213bb5
Pedro Pigatto <pedropigatto54@gmail.com>
2021-11-09 14:23:17 -0300

    Add files via upload
    tell if the year is a leap year or a common year

---
+++ main (3).py
@@ -0,0 +1,12 @@
+﻿year = int(input("Enter a year: "))
+
+if year < 1582:
+    print('Not within the Gregorian calendar period')
+else:
+    if year % 4 != 0 and year % 400 != 0:
+        print('Common year')
+    elif year % 100 != 0 or  year % 400 == 0:
+        print('Leap year')
+
+#if year < 1582:
+ #   print('Not within the Gregorian calendar period')
